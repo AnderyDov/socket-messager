7de031fea2f69630c96e09e5495f7bf4271088d4
Andrey <dovban.andreyy@rambler.ru>
2023-03-09 11:02:01 +0300

    работа с сокетами, бобавлена библиотека moniker

--- client/script.js
+++ client/script.js
@@ -1,57 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
     const socket = io();
 
-    const cookiesArray = document.cookie.split(';');
-    // console.log(cookiesArray);
-
     const username = document.getElementById('username');
     const messageForm = document.getElementById('messageForm');
     const inputMessage = document.getElementById('inputMessage');
     const messages = document.getElementById('messages');
 
-    let token = null;
-
-    if (
-        cookiesArray.filter((item) => item.trim().startsWith('USERNAME='))
-            .length
-    ) {
-        cookiesArray.forEach((el) => {
-            if (el.split('=')[0].trim() === 'USERNAME') {
-                username.innerText = el.split('=')[1].trim().split('TOKEN')[0];
-                token = el.split('=')[1].trim().split('TOKEN')[1];
-            }
-        });
-        console.log('The cookie "USERNAME" exists');
-    } else {
-        let newName = prompt('Введите ваше имя', 'BARBARIS');
-        console.log(newName);
-        if (!newName || newName === '') {
-            newName = 'BARBARIS';
-        }
-
-        username.innerText = newName;
-        document.cookie = `USERNAME=${newName}`;
-        socket.emit('registration', newName);
-
-        socket.on('registration', (msg) => {
-            document.cookie = `USERNAME=${msg}`;
-            token = msg.split('TOKEN')[1];
-        });
-        console.log('The cookie "USERNAME" not found');
-    }
-
     messageForm.addEventListener('submit', (e) => {
         e.preventDefault();
         if (inputMessage.value) {
-            socket.emit('chat message', `${token}TOKEN${inputMessage.value}`);
+            socket.emit('chat message', inputMessage.value);
             inputMessage.value = '';
         }
+    });
+
+    socket.on('set username', (msg) => {
+        username.innerHTML = msg;
     });
 
     socket.on('chat message', (msg) => {
         const item = document.createElement('li');
 
-        item.innerHTML = `${msg.split('TOKEN')[0]}\n${msg.split('TOKEN')[1]}`;
+        item.innerHTML = msg;
         messages.appendChild(item);
         messages.scrollTo(0, messages.scrollHeight);
     });
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const app = express();
 const http = require('http');
-const server = http.createServer(app);
 const { Server } = require('socket.io');
-const io = new Server(server);
-const { v4 } = require('uuid');
+const Moniker = require('moniker');
 
-const users = {};
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/client/index.html');
@@ -14,23 +14,14 @@ app.get('/', (req, res) => {
 app.use(express.static('./client'));
 
 io.on('connection', (socket) => {
-    socket.on('registration', (msg) => {
-        // console.log(msg);
-        const token = v4();
-        users[token] = msg;
-        io.emit('registration', `${msg}TOKEN${token}`);
-    });
+    socket.username = Moniker.choose();
+    socket.emit('set username', socket.username);
+
     socket.on('chat message', (msg) => {
-        // console.log(socket.id, msg);
-        console.log(msg);
-        console.log(users);
-        io.emit(
-            'chat message',
-            `${users[msg.split('TOKEN')[0]]}TOKEN${msg.split('TOKEN')[1]}`,
-        );
+        io.emit('chat message', msg);
     });
 });
 
-server.listen(3000, () => {
+server.listen(PORT, () => {
     console.log('http://localhost:3000');
 });
