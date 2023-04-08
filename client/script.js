da5a2118d189b010e45ac8b6074284a955336520
Andrey <dovban.andreyy@rambler.ru>
2023-04-09 00:18:14 +0300

    fix: исправлен вывод имени пользователя при подключении к чату

--- client/script.js
+++ client/script.js
@@ -1,11 +1,15 @@
+// Когда HTML был загружен ипройден парсером, срабатывает функция
 document.addEventListener('DOMContentLoaded', () => {
+    // Создаётся сокет, не указывается никакого URL-адреса при вызове io(), так как по умолчанию он пытается подключиться к хосту, который обслуживает страницу
     const socket = io();
 
+    // Переменные для обращения с дом элементами
     const username = document.getElementById('username');
     const messageForm = document.getElementById('messageForm');
     const inputMessage = document.getElementById('inputMessage');
     const messages = document.getElementById('messages');
 
+    // Функция доюавления сообщения на страницу, при добавлении сообщения блок с сообщениями прокручивается до самого нзу
     function addMessage(msg) {
         const item = document.createElement('li');
 
@@ -14,6 +18,13 @@ document.addEventListener('DOMContentLoaded', () => {
         messages.scrollTo(0, messages.scrollHeight);
     }
 
+    // Сокет прослушивает событие "set username", вызывает функцию addMessage аргументом msg, это сообщение пришедшее с сервера, также это сообщение прописывается в блок с именем
+    socket.on('set username', (msg) => {
+        addMessage(msg);
+        username.innerHTML = msg.split(' теперь это ваше имя')[0];
+    });
+
+    // при отправек формы, запрещается дефолтное поведение, и при существовании текста в текстовом поле socket испускает событие "chat message" c сообщением тексом из текстового поля
     messageForm.addEventListener('submit', (e) => {
         e.preventDefault();
         if (inputMessage.value) {
@@ -22,12 +33,9 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    socket.on('set username', (msg) => {
-        addMessage(msg);
-        username.innerHTML = msg;
-    });
-
+    // socket прослушивает событие "user join", и вызывает функцию addMessage которой в аргумент попадает сообщение с сервера
     socket.on('user join', addMessage);
 
+    // socket прослушивает событие "chat message", и вызывает функцию addMessage которой в аргумент попадает сообщение с сервера
     socket.on('chat message', addMessage);
 });
--- server.js
+++ server.js
@@ -1,32 +1,42 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const Moniker = require('moniker');
+const express = require('express'); // экспрес сервер
+const http = require('http'); // http nodejs сервер
+const { Server } = require('socket.io'); // класс сервер из библиотеки sovket.io
+const Moniker = require('moniker'); // библиотека для созлания рандомных никнэймов
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
-const PORT = process.env.PORT || 3000;
+const app = express(); // создаётся экземпляр экспресс сервера
+const server = http.createServer(app); // в http сервер передаётся экспресс сервер
+const io = new Server(server); // создаётся экземпляр класса Server который принимет в параметр http сервер
+const PORT = process.env.PORT || 3000; // переменная порт
 
+// из корня отдаётся файл index.html, главная страница
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/client/index.html');
 });
+
+// по этому пути искать статику
 app.use(express.static('./client'));
 
+// io прослушивает дефолтное событие при подключении - "connect", вторым параметром принимает коллбэк с аргументом объект socket
 io.on('connection', (socket) => {
+    // создается рандомный ник и он становится новым свойством объекта socket
     socket.username = Moniker.choose();
+    // socket испускает событие "set username" с сообщением
     socket.emit('set username', `${socket.username} теперь это ваше имя`);
 
+    // socket испускает событие "user join" в режиме broadcast, с сообщением
     socket.broadcast.emit(
         'user join',
         `${socket.username} присоеденился к чату`,
     );
 
+    // socket прослушивает событие "chat message", вторым параметром принимает коллбэк с параметром msg прилитевшем с клиента
     socket.on('chat message', (msg) => {
+        // в коллбэке io  испускает событие "chat message" с прилитевшем сообщением
         io.emit('chat message', msg);
     });
 });
 
+// сервер слушает порт
 server.listen(PORT, () => {
     console.log('http://localhost:3000');
 });
